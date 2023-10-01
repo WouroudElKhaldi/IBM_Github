183bb353f2c9af6cedc0170a8bd1b1862653f6b5
Wouroud El Khaldi <129998266+WouroudElKhaldi@users.noreply.github.com>
2023-10-01 11:57:20 +0300

    Create index.js (script file)

---
+++ index.js
@@ -0,0 +1,20 @@
+function findMaxNumber(arr) {
+  if (arr.length === 0) {
+    return null; // Return null for an empty array
+  }
+
+  let max = arr[0]; // Assume the first element is the maximum
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]; // Update the maximum if we find a larger element
+    }
+  }
+
+  return max;
+}
+
+// Example usage:
+const numbers = [12, 5, 27, 6, 8, 14];
+const maxNumber = findMaxNumber(numbers);
+console.log("The maximum number is:", maxNumber);
